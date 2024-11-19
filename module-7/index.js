fc56c32642d9f1ea63ad37a855f981c32cf93fe3
nhwnahid007 <nhwnahid@gmail.com>
2024-11-20 00:07:16 +0600

    first commit

---
+++ module-7/index.js
@@ -0,0 +1,7 @@
+// const myModule = require('./local-1')
+// console.log(myModule)
+// console.log("Addition of 2 and 3 is:",myModule.add(2,3))
+
+const { a, add } = require("./local-1");
+
+console.log(a);
---
+++ module-7/local-1.js
@@ -0,0 +1,10 @@
+const add = (param1, param2) => param1 + param2;
+
+const a = 10;
+
+module.exports = {
+  a,
+  add,
+};
+
+// console.log(module)
